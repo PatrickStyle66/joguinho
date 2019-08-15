e6cd45329005d566b3eee930813705281702d639
PatrickStyle66 <jphamega@hotmail.com>
2019-08-15 00:44:03 -0300

    versão q deveria ter sido upada >:(

--- sidescrollerP4/main.py
+++ sidescrollerP4/main.py
@@ -16,7 +16,7 @@ W, H = 928, 591
 
 win = pygame.display.set_mode((W, H))
 
-pygame.display.set_caption('Side Scroller')
+pygame.display.set_caption('Dummy Runner!')
 
 bg = pygame.image.load(os.path.join('images', 'bg.png')).convert()
 
@@ -26,6 +26,7 @@ bgX2 = bg.get_width()
 
 clock = pygame.time.Clock()
 
+chayenne = pygame.image.load(os.path.join('images','chayenne.jpg'))
 
 class player(object):
     run = [pygame.image.load(os.path.join('images', str(x) + '.png')) for x in range(8, 16)]
@@ -155,7 +156,7 @@ class saw(object):
 class spike(saw):
     img = pygame.image.load(os.path.join('images','spike.png'))
     def draw(self,win):
-        self.hitbox = (self.x + 12, self.y,24,469)
+        self.hitbox = (self.x + 12, self.y,24,470)
         win.blit(self.img,(self.x,self.y))
         #pygame.draw.rect(win,(255,0,0),self.hitbox,2)
 
@@ -163,6 +164,37 @@ class spike(saw):
         if rect[0] + rect[2] > self.hitbox[0] and rect[0] < self.hitbox[0] + self.hitbox[2]:
             if rect[1] < self.hitbox[3]:
                 return True
+        return False
+
+
+class button():
+    def __init__(self, color, x, y, width, height, text=''):
+        self.color = color
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.text = text
+
+    def draw(self, win, outline=None):
+
+        if outline:
+            pygame.draw.rect(win, outline, (self.x - 2, self.y - 2, self.width + 4, self.height + 4), 0)
+
+        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height), 0)
+
+        if self.text != '':
+            font = pygame.font.SysFont('comicsans', 50)
+            text = font.render(self.text, 1, (0, 0, 0))
+            win.blit(text, (
+            self.x + (self.width / 2 - text.get_width() / 2), self.y + (self.height / 2 - text.get_height() / 2)))
+
+    def isOver(self, pos):
+
+        if pos[0] > self.x and pos[0] < self.x + self.width:
+            if pos[1] > self.y and pos[1] < self.y + self.height:
+                return True
+
         return False
 
 def redrawWindow():
@@ -194,30 +226,86 @@ def endScreen():
     pause = 0
     objects = []
     speed = 60
-
+    tryagain = button((0,255,0), 350,420,250,100,'Reiniciar')
     run = True
     while run:
         pygame.time.delay(100)
+        tryagain.draw(win,(0,0,0))
+        pygame.display.update()
         for event in pygame.event.get():
+            pos = pygame.mouse.get_pos()
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                run = False
+                if tryagain.isOver(pos):
+                    run = False
         win.blit(bg,(0,0))
         largeFont = pygame.font.SysFont('comicsans',80)
         mediumFont = pygame.font.SysFont('comicsans',30)
-        deathmsg = largeFont.render('O corno morreu kjkjkjkkk',1,(255,255,255))
+        deathmsg = largeFont.render('Morreu!!',1,(255,255,255))
         win.blit(deathmsg,(W/2 - deathmsg.get_width()/2, 100))
         previousScore = largeFont.render('Melhor Pontuação: ' +str(updateFile()),1,(255,255,255))
         win.blit(previousScore,(W/2 - previousScore.get_width()/2,200))
         newScore = largeFont.render('Pontuação Final: ' + str(score),1,(255,255,255))
         win.blit(newScore, (W / 2 - newScore.get_width() / 2, 320))
-        tryagain = mediumFont.render('(clique na tela para jogar novamente)',1,(255,255,255))
-        win.blit(tryagain,(W/2 - tryagain.get_width()/2,420))
-        pygame.display.update()
+        #tryagain = mediumFont.render('(clique na tela para jogar novamente)',1,(255,255,255))
+        #win.blit(tryagain,(W/2 - tryagain.get_width()/2,420))
+        #pygame.display.update()
     score = 0
     runner.falling = False
+
+def creditScreen():
+    run = True
+    back = button((0,255,0),600,25,250 ,100,'Voltar')
+    font = pygame.font.SysFont('comicsans', 30)
+    patrick = font.render('-Jonathas Patrick H. de Azevedo | jpha@ic.ufal.br',1,(255,255,255))
+    thalyssa = font.render('-Thalyssa de Almeida Monteiro | tam@ic.ufal.br',1,(255,255,255))
+    cat = font.render('...e chayenne ^ ^',1,(255,255,255))
+    copyright = font.render('©2019 Thalick games',1,(255,255,255))
+    while run:
+        pygame.time.delay(100)
+        back.draw(win,(0,0,0))
+        pygame.display.update()
+        for event in pygame.event.get():
+            pos = pygame.mouse.get_pos()
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if back.isOver(pos):
+                    run = False
+        win.blit(bg, (0, 0))
+        win.blit(patrick, (0, 100))
+        win.blit(thalyssa, (0, 250))
+        win.blit(cat, (0, 350))
+        win.blit(chayenne,(10,380))
+        win.blit(copyright,(650,550))
+def menu():
+    run = True
+    start = button((0,255,0),350,250,250,100,'Jogar')
+    credit = button((0,255,0),350,400,250,100,'Créditos')
+    font = pygame.font.SysFont('minecrafteralt',100)
+    title = font.render('Dummy Runner',1,(255,255,255))
+    while run:
+        pygame.time.delay(100)
+        start.draw(win,(0,0,0))
+        credit.draw(win,(0,0,0))
+        pygame.display.update()
+        for event in pygame.event.get():
+            pos = pygame.mouse.get_pos()
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if start.isOver(pos):
+                    run = False
+                if credit.isOver(pos):
+                    creditScreen()
+
+        win.blit(bg, (0, 0))
+        win.blit(title,(W / 2 - title.get_width() / 2, 50))
+
 
 runner = player(200,468,64,64)
 pygame.time.set_timer(USEREVENT+1,1000)
@@ -229,6 +317,8 @@ pause = 0
 fallSpeed = 0
 
 objects = []
+
+menu()
 
 while run:
     score = speed//5 - 12
